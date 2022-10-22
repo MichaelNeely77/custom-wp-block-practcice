869b18c4ff8a0a74d660e7f65a35632eeb53d5f7
Michael Neely <michael@michaelneely.net>
2022-10-22 18:12:20 -0400

    Adding more React

--- neely-block.js
+++ neely-block.js
@@ -12,7 +12,35 @@ wp.blocks.registerBlockType('neely/custom-block',{
         commpanyZip: {type:'string'}
     },
     edit: function(props){
-        return
+        return React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Company Name"), /*#__PURE__*/React.createElement("input", {
+            type: "text",
+            value: "",
+            placeholder: "Company Name"
+          }), /*#__PURE__*/React.createElement("label", null, "Company Phone"), /*#__PURE__*/React.createElement("input", {
+            type: "text",
+            value: "",
+            placeholder: "Company Phone"
+          }), /*#__PURE__*/React.createElement("label", null, "Company Address"), /*#__PURE__*/React.createElement("input", {
+            type: "text",
+            value: "",
+            placeholder: "Company Address"
+          }), /*#__PURE__*/React.createElement("label", null, "Company Address 2"), /*#__PURE__*/React.createElement("input", {
+            type: "text",
+            value: "",
+            placeholder: "Company Address 2"
+          }), /*#__PURE__*/React.createElement("label", null, "Company City"), /*#__PURE__*/React.createElement("input", {
+            type: "text",
+            value: "",
+            placeholder: "Company City"
+          }), /*#__PURE__*/React.createElement("label", null, "Company State"), /*#__PURE__*/React.createElement("input", {
+            type: "text",
+            value: "",
+            placeholder: "Company State"
+          }), /*#__PURE__*/React.createElement("label", null, "Company Zip Code"), /*#__PURE__*/React.createElement("input", {
+            type: "text",
+            value: "",
+            placeholder: "Company Zip Code"
+          }));
     },
     save: function(props){
 
