60c8d149929d580478f4214af1a83a80a1e7e318
Michael Neely <michael@michaelneely.net>
2022-10-22 18:53:58 -0400

    Added teh save function

--- neely-block.js
+++ neely-block.js
@@ -59,6 +59,42 @@ wp.blocks.registerBlockType('neely/custom-block',{
           })));
     },
     save: function(props){
-        return null;
+        return React.createElement("div", null, /*#__PURE__*/React.createElement("h3", null, props.attributes.companyName), /*#__PURE__*/React.createElement("div", null, props.attributes.companyPhone), /*#__PURE__*/React.createElement("span", null, props.attributes.companyAddress), /*#__PURE__*/React.createElement("span", null, props.attributes.companyAddress2), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("span", null, props.attributes.companyCity), /*#__PURE__*/React.createElement("span", null, props.attributes.companyState), /*#__PURE__*/React.createElement("span", null, props.attributes.companyZip)));
     }
 })
+
+/**
+ * 
+ * HTML for form that was converte4d to Reactjs in Babeljs.io
+ * 
+ * <div>
+  <div>
+<label>Company Name</label><br />
+<input type="text" value={props.attributes.companyName} placeholder="Company Name" onChange={updateCompanyName} />
+  </div>
+  <div>
+<label>Company Phone</label><br />
+<input type="text" value={props.attributes.companyPhone} placeholder="Company Phone" onChange={updateCompanyPhone} />
+  </div>
+  <div>
+<label>Company Address</label><br />
+<input type="text" value={props.attributes.companyAddress} placeholder="Company Address" onChange={updateCompanyAddress} />
+  </div>
+  <div>
+<label>Company Address 2</label><br />
+<input type="text" value={props.attributes.companyAddress2} placeholder="Company Address 2" onChange={updateCompanyAddress2} />
+  </div>
+  <div>
+  <label>Company City</label><br />
+<input type="text" value={props.attributes.companyCity} placeholder="Company City" onChange={updateCompanyCity} />
+  </div>
+  <div>
+  <label>Company State</label><br />
+<input type="text" value={props.attributes.companyState} placeholder="Company State" onChange={updateCompanyState} />
+  </div>
+  <div>
+  <label>Company Zip Code</label><br />
+<input type="text" value={props.attributes.companyZip} placeholder="Company Zip Code" onChange={updateCompanyZip} />
+  </div>
+</div>
+ */
