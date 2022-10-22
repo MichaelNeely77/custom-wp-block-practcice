c3b31774f1e605c32affa98f348132e5fbbd19a7
Michael Neely <michael@michaelneely.net>
2022-10-22 18:19:56 -0400

    Added page breaks to the form

--- neely-block.js
+++ neely-block.js
@@ -12,37 +12,37 @@ wp.blocks.registerBlockType('neely/custom-block',{
         commpanyZip: {type:'string'}
     },
     edit: function(props){
-        return React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Company Name"), /*#__PURE__*/React.createElement("input", {
+        return React.createElement("div", null, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Company Name"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
             type: "text",
             value: "",
             placeholder: "Company Name"
-          }), /*#__PURE__*/React.createElement("label", null, "Company Phone"), /*#__PURE__*/React.createElement("input", {
+          })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Company Phone"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
             type: "text",
             value: "",
             placeholder: "Company Phone"
-          }), /*#__PURE__*/React.createElement("label", null, "Company Address"), /*#__PURE__*/React.createElement("input", {
+          })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Company Address"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
             type: "text",
             value: "",
             placeholder: "Company Address"
-          }), /*#__PURE__*/React.createElement("label", null, "Company Address 2"), /*#__PURE__*/React.createElement("input", {
+          })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Company Address 2"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
             type: "text",
             value: "",
             placeholder: "Company Address 2"
-          }), /*#__PURE__*/React.createElement("label", null, "Company City"), /*#__PURE__*/React.createElement("input", {
+          })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Company City"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
             type: "text",
             value: "",
             placeholder: "Company City"
-          }), /*#__PURE__*/React.createElement("label", null, "Company State"), /*#__PURE__*/React.createElement("input", {
+          })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Company State"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
             type: "text",
             value: "",
             placeholder: "Company State"
-          }), /*#__PURE__*/React.createElement("label", null, "Company Zip Code"), /*#__PURE__*/React.createElement("input", {
+          })), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", null, "Company Zip Code"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
             type: "text",
             value: "",
             placeholder: "Company Zip Code"
-          }));
+          })));
     },
     save: function(props){
-
+        return null;
     }
 })
