72b2c6dd559968f7db847e347710fd12c52f8cf8
Michael Neely <michael@michaelneely.net>
2022-10-22 17:08:00 -0400

    Adding the JS file and the initial WP Block framework

--- neely-custom-block.php
+++ neely-custom-block.php
@@ -8,7 +8,7 @@
  */
 
  function neely_custom_block_script_register() {
-
+    wp_enqueue_script('neely-custom-block', plugin_dir_url(__FILE__).'neely-block.js', array('wp-blocks','wp-i18n','wp-editor'),true,false);
  
  }
  add_action('enqueue_block_editor_assets', 'neely_custom_block_script_register');
